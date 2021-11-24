0a93614a929f4c3c4ce011f741f5e2c0db76d576
Doğukan <dogu_kutluay_1999@hotmail.com>
2021-11-24 23:57:00 +0300

    Provider created for redux state registration

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
+
+import { createBrowserHistory } from 'history';
+import { Router } from 'react-router-dom';
+
+import { Provider as ReduxStoreProvider } from 'react-redux';
+import configureStore from './providers/reduxConfigureStore';
+
+const history = createBrowserHistory();
+
+const store = configureStore();
+
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <ReduxStoreProvider store={store}>
+    <Router history={history}>
+      <App />
+    </Router>
+  </ReduxStoreProvider>,
   document.getElementById('root')
 );
